b387123704d0493c5666fd855a0fc49901ba7e37
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-09-20 17:00:21 +0200

    fixed secret key issue
    env var must be at the same level as package.json!

--- src/store/index.js
+++ src/store/index.js
@@ -44,7 +44,7 @@ const persistedReducer = persistReducer(persistConfig, bigReducer);
 export const configureStore = createStore(
   persistedReducer,
   initialState,
-  composeEnhancers(applyMiddleware(thunk))
+  process.env.REACT_APP_DEVELOPMENT ? composeEnhancers(applyMiddleware(thunk)) : compose(applyMiddleware(thunk))
 );
 
 // ! dont forget
